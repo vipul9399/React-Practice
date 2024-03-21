c104c5b944fc005d5dfb402d635c8f2b5c30a5b8
vipul9399 <vipulpatil73@gmail.com>
2024-03-21 13:50:39 +0530

    Components, JSX, Props, State understanding

---
+++ react-essentials-start/src/components/Tabs.jsx
@@ -0,0 +1,11 @@
+export default function Tabs({children, buttons, buttonsContainer}) {
+    const ButtonContainer = buttonsContainer;
+  return (
+    <>
+      <ButtonContainer>
+        {buttons}
+      </ButtonContainer>
+      {children}
+    </>
+  );
+}
